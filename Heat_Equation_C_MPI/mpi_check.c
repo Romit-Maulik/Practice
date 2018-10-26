6bb3e9986c93aabd795cb35e481b53931ed4be98
Romit Maulik <romit.maulik@okstate.edu>
2018-10-25 20:04:29 -0500

    Add files via upload

--- Heat_Equation_C_MPI/mpi_check.c
+++ Heat_Equation_C_MPI/mpi_check.c
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
 	ierr = MPI_Init(&argc, &argv);
 
 	/* find out MY process ID, and how many processes were started. 
-	   Note that num_procs should be divisible by 4
+	   Note that num_procs should be a square of two integers
 	*/
 	ierr = MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
 	ierr = MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
