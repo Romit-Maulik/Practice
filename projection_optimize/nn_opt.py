Fixed scaling bug

projection_optimize/nn_opt.py
@@ -6,32 +6,27 @@ import numpy as np
 np.random.seed(15)
 import matplotlib.pyplot as plt
 
-from surrogate_models import coefficient_model, coefficient_model_adjoint
+from surrogate_models import coefficient_model
 from optimizers import surrogate_optimizer
 from utils import shape_return
 
 if __name__ == '__main__':
 
     '''
-    Usage: python nn_opt.py [train/optimize/rl_train/rl_optimize] [regular/augmented]
+    Usage: python nn_opt.py [train/optimize/rl_train/rl_optimize] [shape/shape_lift]
     '''
 
-    parser = argparse.ArgumentParser(description='Adjoint augmented surrogate model based optimization')
-    parser.add_argument('mode', metavar='mode', type=str, help='[train/optimize/rl_optimize]') #Train a network or use a trained network for minimize/rl_minimize
+    parser = argparse.ArgumentParser(description='Surrogate model based optimization')
+    parser.add_argument('mode', metavar='mode', type=str, help='[train/optimize/rl_train/rl_optimize]') #Train a network or use a trained network for minimize/rl_minimize
     parser.add_argument('constraint_options',metavar='constraint_options', type=str, help='[shape/shape_lift]')
-    parser.add_argument('adjoint_mode', metavar='adjoint_mode', type=str, help='[regular/augmented]')
     args = parser.parse_args()
 
     # Load dataset
     input_data = np.load('DOE_2000.npy').astype('float32')
     output_data = np.load('coeff_data_2000.npy').astype('float32')
-    adjoint_data = np.zeros(shape=(2000,8)).astype('float32') # placeholder
 
     # Define a simple fully connected model
-    if args.adjoint_mode == 'augmented':
-        model=coefficient_model_adjoint(input_data,output_data,adjoint_data)
-    else:
-        model=coefficient_model(input_data,output_data)
+    model=coefficient_model(input_data,output_data)
 
     # Train the model
     if args.mode == 'train':
@@ -52,7 +47,7 @@ if __name__ == '__main__':
         else:
             opt = surrogate_optimizer(model,num_pars,cons)
         
-        best_func_val, solution, best_opt = opt.optimize(30) # Optimize with 10 restarts
+        best_func_val, solution, best_opt = opt.optimize(10) # Optimize with 10 restarts
         
         # Visualize airfoil shape evolution
         for i in range(1,np.shape(best_opt)[0]):
@@ -67,10 +62,9 @@ if __name__ == '__main__':
         env_params['num_params'] = np.shape(input_data)[1]
         env_params['num_obs'] = np.shape(output_data)[1]
         env_params['init_guess'] = np.asarray(t_base)
-        env_params['model_type'] = 'regular'
-        env_params['num_steps'] = 5 # 10 step iteration
+        env_params['num_steps'] = 1 # 1 step prediction
 
-        rl_opt = rl_optimize(env_params,args.adjoint_mode,50,env_params['num_steps']) # 20 iteration optimization, 10 steps
+        rl_opt = rl_optimize(env_params,50,env_params['num_steps']) # 20 iteration optimization, 10 steps
         rl_opt.train()
 
     elif args.mode == 'rl_optimize':
@@ -82,10 +76,9 @@ if __name__ == '__main__':
         env_params['num_params'] = np.shape(input_data)[1]
         env_params['num_obs'] = np.shape(output_data)[1]
         env_params['init_guess'] = np.asarray(t_base,dtype='float32')
-        env_params['model_type'] = 'regular'
-        env_params['num_steps'] = 5
+        env_params['num_steps'] = 1
 
-        rl_opt = rl_optimize(env_params,args.adjoint_mode,50,env_params['num_steps']) # 20 iteration optimization, 10 steps
+        rl_opt = rl_optimize(env_params,50,env_params['num_steps']) # 20 iteration optimization, 10 steps
 
         f = open('rl_checkpoint','r')
         checkpoint_path = f.readline()

projection_optimize/optimizers.py
@@ -5,12 +5,6 @@ np.random.seed(10)
 from scipy.optimize import minimize
 from constraints import t_lower, t_upper
 
-temp_lift = 1.1
-# For lift constraint - constrained at 1.0
-def lift_eq_cons(input_var):
-    global temp_lift
-    return temp_lift-0.8
-
 class surrogate_optimizer():
     def __init__(self,trained_model,num_pars,cons,lift_cons=False):
         self.model = trained_model
@@ -27,6 +21,38 @@ class surrogate_optimizer():
 
         self.scaler_min = tf.convert_to_tensor(trained_model.op_scaler.data_min_,dtype='float32') # Required for rescaling within TF
         self.scaler_max = tf.convert_to_tensor(trained_model.op_scaler.data_max_,dtype='float32')
+
+        self.target_lift = 1.0
+
+    # For lift constraint - constrained at self.target_lift
+    def lift_eq_cons(self,input_var):
+        input_var = input_var.reshape(1,self.num_pars).astype('float32')
+        input_var = tf.Variable(input_var)
+            
+        op = self.model(input_var)
+        op = op*(self.scaler_max-self.scaler_min) + self.scaler_min
+        # op = op*(self.scaler_var)+self.scaler_mean
+
+        pred = (op[0][1]-self.target_lift).numpy()
+            
+        return pred
+
+    def jac_equality(self,input_var):
+        input_var = input_var.reshape(1,self.num_pars).astype('float32')
+        input_var = tf.Variable(input_var)
+            
+        with tf.GradientTape(persistent=True) as t:
+            t.watch(input_var)
+
+            op = self.model(input_var)
+            op = op*(self.scaler_max-self.scaler_min) + self.scaler_min
+            # op = op*(self.scaler_var)+self.scaler_mean
+
+            # This is the objective function (square of the first variable out)
+            pred = (op[0][1]-self.target_lift)
+            
+        return t.gradient(pred, input_var).numpy()[0,:].astype('double')
+
 
     def jac_method(self,input_var):
         input_var = input_var.reshape(1,self.num_pars).astype('float32')
@@ -64,7 +90,7 @@ class surrogate_optimizer():
 
     def single_optimize(self,init_guess):
         if self.lift_cons_append:
-            self.cons.append({'type': 'eq', 'fun': lift_eq_cons})
+            self.cons.append({'type': 'eq', 'fun': self.lift_eq_cons, 'jac': self.jac_equality})
             self.lift_cons_append = False
 
         self.solution = minimize(self.residual,init_guess,

projection_optimize/rl_optimizers.py
@@ -16,7 +16,7 @@ from ray.tune.registry import register_env
 import gym
 from gym.spaces import Box
 
-from surrogate_models import coefficient_model, coefficient_model_adjoint
+from surrogate_models import coefficient_model
 from surrogate_environments import airfoil_surrogate_environment
 
 tf = try_import_tf()
@@ -43,7 +43,7 @@ class CustomModel(TFModelV2):
 
 
 class rl_optimize():
-    def __init__(self,env_params,model_args,num_iters,num_steps):
+    def __init__(self,env_params,num_iters,num_steps):
         # Load dataset
         input_data = np.load(dir_path+'/DOE_2000.npy').astype('float32')
         output_data = np.load(dir_path+'/coeff_data_2000.npy').astype('float32')
@@ -51,11 +51,7 @@ class rl_optimize():
         self.obs_shape = np.shape(output_data)[1]
 
         # Reload model because of TF2/Ray issues
-        if model_args == 'regular':
-            self.model=coefficient_model(input_data,output_data)
-        elif model_args == 'augmented':
-            adjoint_data = np.zeros(shape=(2000,8)).astype('float32') # placeholder
-            self.model=coefficient_model_adjoint(input_data,output_data,adjoint_data)
+        self.model=coefficient_model(input_data,output_data)
 
         ray.init()
 

projection_optimize/surrogate_environments.py
@@ -4,7 +4,7 @@ import gym
 import numpy as np
 np.random.seed(10)
 from gym import spaces
-from surrogate_models import coefficient_model, coefficient_model_adjoint
+from surrogate_models import coefficient_model
 from constraints import t_lower, t_upper
 
 """
@@ -26,11 +26,7 @@ class airfoil_surrogate_environment(gym.Env):
         input_data = np.load(dir_path+'/DOE_2000.npy').astype('float32')
         output_data = np.load(dir_path+'/coeff_data_2000.npy').astype('float32')
 
-        if env_params['model_type'] == 'regular':
-            self.model = coefficient_model(input_data,output_data)
-        elif env_params['model_type'] == 'augmented':
-            adjoint_data = np.zeros(shape=(2000,8)).astype('float32') # placeholder
-            self.model = coefficient_model_adjoint(input_data,output_data,adjoint_data)
+        self.model = coefficient_model(input_data,output_data)
 
         # Restore model for use in RL
         self.model.restore_model()
@@ -105,7 +101,6 @@ if __name__ == '__main__':
     env_params['num_params'] = 8
     env_params['num_obs'] = 2
     env_params['init_guess'] = np.random.uniform(size=(27))
-    env_params['model_type'] = 'regular'
     env_params['num_steps'] = 5
 
     check = airfoil_surrogate_environment(env_params)

projection_optimize/surrogate_models.py
@@ -8,7 +8,7 @@ from tensorflow.keras import Model
 import numpy as np
 np.random.seed(10)
 
-from utils import coeff_determination
+from utils import coeff_determination, plot_scatter, plot_stats
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 #Build the model which does basic map of inputs to coefficients
@@ -19,21 +19,25 @@ class coefficient_model(Model):
         # Scale
         # self.op_scaler = StandardScaler()
         self.op_scaler = MinMaxScaler()
-        self.op_scaler.fit_transform(output_data)
+        output_data = self.op_scaler.fit_transform(output_data)
 
         # Randomize datasets
         idx = np.arange(np.shape(input_data)[0])
         np.random.shuffle(idx)
 
+        self.ntrain = 45
+        self.nvalid = 5
+        self.num_batches = 15 # Set to 3 points per batch for train data
+
         # Segregate
-        self.input_data_train = input_data[idx[:50]]
-        self.output_data_train = output_data[idx[:50]]
+        self.input_data_train = input_data[idx[:self.ntrain]]
+        self.output_data_train = output_data[idx[:self.ntrain]]
 
-        self.input_data_valid = input_data[idx[50:100]]
-        self.output_data_valid = output_data[idx[50:100]]
+        self.input_data_valid = input_data[idx[self.ntrain:self.ntrain+self.nvalid]]
+        self.output_data_valid = output_data[idx[self.ntrain:self.ntrain+self.nvalid]]
 
-        self.input_data_test = input_data[idx[100:]]
-        self.output_data_test = output_data[idx[100:]]
+        self.input_data_test = input_data[idx[self.ntrain+self.nvalid:]]
+        self.output_data_test = output_data[idx[self.ntrain+self.nvalid:]]
 
         self.ip_shape = np.shape(input_data)[1]
         self.op_shape = np.shape(output_data)[1]
@@ -59,6 +63,17 @@ class coefficient_model(Model):
         boom=self.call(X)
         return tf.reduce_mean(tf.math.square(boom-Y))
 
+    #     # Adjoint enhanced MSE
+    # def get_loss(self,X,Y,A):
+    #     input_var = tf.Variable(X.astype('float32'))
+    #     with tf.GradientTape() as tape:
+    #         tape.watch(input_var)
+    #         boom=self.call(input_var)
+    #         op = tf.math.square(boom)[0][0]
+    #         g = tf.reduce_sum(tf.square(tape.gradient(op,input_var)-A))
+
+    #     return tf.reduce_mean(tf.math.square(boom-Y)) + g
+
     # get gradients - regular
     def get_grad(self,X,Y):
         with tf.GradientTape() as tape:
@@ -76,30 +91,50 @@ class coefficient_model(Model):
     def train_model(self):
         plot_iter = 0
         stop_iter = 0
-        patience = 50
+        patience = 20
         best_valid_loss = 999999.0 # Some large number 
+
+        self.train_batch_size = int(self.ntrain/self.num_batches)
+        self.valid_batch_size = int(self.nvalid/self.num_batches)
+
+        f = open("training_stats.csv","w")
+        f.write('Train loss, Validation loss, Train R2, Validation R2'+'\n')
+        f.close()
         
         for i in range(2000):
             # Training loss
-            for batch in range(10):
-                input_batch = self.input_data_train[batch*5:(batch+1)*5]
-                output_batch = self.output_data_train[batch*5:(batch+1)*5]
+            train_loss = 0.0
+            train_r2 = 0.0
+            for batch in range(self.num_batches):
+                input_batch = self.input_data_train[batch*self.train_batch_size:(batch+1)*self.train_batch_size]
+                output_batch = self.output_data_train[batch*self.train_batch_size:(batch+1)*self.train_batch_size]
                 self.network_learn(input_batch,output_batch)
 
+                train_loss = train_loss + self.get_loss(input_batch,output_batch).numpy()
+                predictions = self.call(input_batch)
+                train_r2 = train_r2 + coeff_determination(predictions,output_batch)
+
+            train_r2 = train_r2/(batch+1)
+
             # Validation loss
-            valid_loss = 0.0
-            valid_r2 = 0.0
-            for batch in range(10):
-                input_batch = self.input_data_valid[batch*5:(batch+1)*5]
-                output_batch = self.output_data_valid[batch*5:(batch+1)*5]
-                valid_loss = valid_loss + self.get_loss(self.input_data_valid,self.output_data_valid).numpy()
-
+            if self.valid_batch_size == 0:
+                valid_loss = self.get_loss(self.input_data_valid,self.output_data_valid).numpy()
                 predictions = self.call(self.input_data_valid)
-                valid_r2 = valid_r2 + coeff_determination(predictions,self.output_data_valid)
+                valid_r2 = coeff_determination(predictions,self.output_data_valid)
+            else:
+                # Validation loss
+                valid_loss = 0.0
+                valid_r2 = 0.0
+                for batch in range(self.num_batches):
+                    input_batch = self.input_data_valid[batch*self.valid_batch_size:(batch+1)*self.valid_batch_size]
+                    output_batch = self.output_data_valid[batch*self.valid_batch_size:(batch+1)*self.valid_batch_size]
+                    
+                    valid_loss = valid_loss + self.get_loss(input_batch,output_batch).numpy()
+                    predictions = self.call(input_batch)
+                    valid_r2 = valid_r2 + coeff_determination(predictions,output_batch)
 
-            valid_r2 = valid_r2/(batch+1)
+                valid_r2 = valid_r2/(batch+1)
                 
-
             # Check early stopping criteria
             if valid_loss < best_valid_loss:
                 
@@ -112,6 +147,11 @@ class coefficient_model(Model):
             else:
                 stop_iter = stop_iter + 1
 
+            # Write metrics to file
+            f = open("training_stats.csv","a")
+            f.write(str(train_loss)+','+str(valid_loss)+','+str(train_r2)+','+str(valid_r2)+'\n')
+            f.close()
+
             if stop_iter == patience:
                 break
                 
@@ -120,116 +160,32 @@ class coefficient_model(Model):
         print('Test loss:',self.get_loss(self.input_data_test,self.output_data_test).numpy())
         r2 = coeff_determination(predictions,self.output_data_test)
         print('Test R2:',r2)
-        r2_iter = 0            
-
-    # Load weights
-    def restore_model(self):
-        self.load_weights(dir_path+'/checkpoints/my_checkpoint') # Load pretrained model
-
-
-#Build the model which predicts coefficients enhanced by adjoint information based loss
-class coefficient_model_adjoint(Model):
-    def __init__(self,input_data,output_data,adjoint_data):
-        super(coefficient_model_adjoint, self).__init__()
-
-        # Scale
-        self.op_scaler = MinMaxScaler()
-        self.op_scaler.fit_transform(output_data)
-
-        # Randomize
-        idx = np.arange(np.shape(input_data)[0])
-        np.random.shuffle(idx)
-
-        # Segregate
-        self.input_data_train = input_data[idx[:140]]
-        self.output_data_train = output_data[idx[:140]]
-        self.adjoint_data_train = adjoint_data[idx[:140]]
-
-        self.input_data_test = input_data[idx[140:]]
-        self.output_data_test = output_data[idx[140:]]
-        self.adjoint_data_test = adjoint_data[idx[140:]]
-
-        # Shapes
-        self.ip_shape = np.shape(input_data)[1]
-        self.op_shape = np.shape(output_data)[1]
-
-        # Define model
-        xavier=tf.keras.initializers.GlorotUniform()
-        self.l1=tf.keras.layers.Dense(20,kernel_initializer=xavier,activation=tf.nn.tanh,input_shape=[self.ip_shape])
-        self.l2=tf.keras.layers.Dense(20,kernel_initializer=xavier,activation=tf.nn.tanh)
-        self.l3=tf.keras.layers.Dense(20,kernel_initializer=xavier,activation=tf.nn.tanh)
-        self.out=tf.keras.layers.Dense(self.op_shape,kernel_initializer=xavier,activation=tf.nn.tanh)
-        self.train_op = tf.keras.optimizers.Adam(learning_rate=0.001)
-    
-    # Running the model
-    def call(self,X):
-        boom=self.l1(X)
-        boom=self.l2(boom)
-        boom=self.l3(boom)
-        boom=self.out(boom)
-        return boom
-    
-    # Adjoint enhanced MSE
-    def get_loss(self,X,Y,A):
-        input_var = tf.Variable(X.astype('float32'))
-        with tf.GradientTape() as tape:
-            tape.watch(input_var)
-            boom=self.call(input_var)
-            op = tf.math.square(boom)[0][0]
-            g = tf.reduce_sum(tf.square(tape.gradient(op,input_var)-A))
-
-        return tf.reduce_mean(tf.math.square(boom-Y)) + g
-
-    # get gradients - adjoint enhanced
-    def get_grad(self,X,Y,A):
-        with tf.GradientTape() as tape:
-            tape.watch(self.trainable_variables)
-            L = self.get_loss(X,Y,A)
-            g = tape.gradient(L, self.trainable_variables)
-        return g
-
-    # perform gradient descent - adjoint enhanced
-    def network_learn(self,X,Y,A):
-        g = self.get_grad(X,Y,A)
-        self.train_op.apply_gradients(zip(g, self.trainable_variables))
-
-    # Train the model
-    def train_model(self):
-        plot_iter = 0
         r2_iter = 0
-        for i in range(200):
-            for batch in range(7):
-                input_batch = self.input_data_train[batch*20:(batch+1)*20]
-                output_batch = self.output_data_train[batch*20:(batch+1)*20]
-                adjoint_batch = self.adjoint_data_train[batch*20:(batch+1)*20]
-                
-                self.network_learn(input_batch,output_batch,adjoint_batch)
 
-            # Check accuracy
-            if r2_iter == 10:
-                print('Test loss:',self.get_loss(self.input_data_test,self.output_data_test,self.adjoint_data_test))
-                predictions = self.call(self.input_data_test)
-                r2 = coeff_determination(predictions,self.output_data_test)
-                print('Test R2:',r2)
-                r2_iter = 0
-                self.save_weights('./checkpoints/my_checkpoint')
-            else:
-                r2_iter = r2_iter + 1
+        # Plot scatter on the test
+        predictions = self.op_scaler.inverse_transform(predictions)
+        truth = self.op_scaler.inverse_transform(self.output_data_test)
+        plot_scatter(predictions[:,0],truth[:,0],'Drag')
+        plot_scatter(predictions[:,1],truth[:,1],'Lift')
+
+        # Plot training stats
+        plot_stats()
 
     # Load weights
     def restore_model(self):
         self.load_weights(dir_path+'/checkpoints/my_checkpoint') # Load pretrained model
+
 
 if __name__ == '__main__':
     # Load dataset
-    input_data = np.load('doe_data.npy').astype('float32')
-    output_data = np.load('coeff_data.npy').astype('float32')
+    input_data = np.load('DOE_2000.npy').astype('float32')
+    output_data = np.load('coeff_data_2000.npy').astype('float32')
     # Define a simple fully connected model
     model=coefficient_model(input_data,output_data)
     # Restore
     model.restore_model()
     # Predict
-    inputs = np.asarray([0.1009, 0.3306, 0.6281, 0.1494, -0.1627, -0.6344, -0.5927, 0.0421]).astype('float32')
+    inputs = np.asarray([0.08879032,  0.32760656,  0.64642604,  0.18388863, -0.16479598, -0.58546438, -0.42737799,  0.04069137]).astype('float32')
     inputs = inputs.reshape(1,8)
-    pred = model.predict(inputs)
-    print(pred)
+    pred = model.op_scaler.inverse_transform(model.predict(inputs))
+    print("%.6f" % pred[0][0], "%.6f" % pred[0][1])

projection_optimize/utils.py
@@ -9,7 +9,7 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 if not os.path.exists(dir_path+'/Shapes/'):
     os.mkdir(dir_path+'/Shapes')
 else:
-    files = glob.glob(dir_path+'/Shapes/*.jpeg')
+    files = glob.glob(dir_path+'/Shapes/*.png')
     for f in files:
         os.remove(f)
 
@@ -47,9 +47,14 @@ def shape_return(a,pnum):
 
     xx, cc = plot_arrays(a,x0,x1,p)
 
-    plt.figure()
-    plt.plot(xx,cc,label='Found')
-    plt.ylim((-0.1,0.1))
+    if pnum == 166:       
+        plt.figure()
+        plt.plot(xx,cc,label='Optimized!')
+        plt.ylim((-0.1,0.1))
+    else:
+        plt.figure()
+        plt.plot(xx,cc,label='Iteration '+str(pnum))
+        plt.ylim((-0.1,0.1))
 
     from constraints import t_base   
     t_base = np.asarray(t_base)
@@ -58,7 +63,7 @@ def shape_return(a,pnum):
     
     plt.plot(xx,cc,label='Base')
     plt.legend()
-    plt.savefig('Shapes/Shape_'+str(pnum)+'.jpeg')
+    plt.savefig('Shapes/Shape_'+"{0:0>2}".format(pnum)+'.png')
     plt.close()
 
 
@@ -96,16 +101,67 @@ def shape_return_comparison(a,b,pnum):
     plt.savefig('Shapes/Shape_'+str(pnum)+'.jpeg')
     plt.close()
 
+def plot_stats():
+    import matplotlib
+    matplotlib.rcParams['mathtext.fontset'] = 'stix'
+    matplotlib.rcParams['font.family'] = 'STIXGeneral'
+    
+    data = np.loadtxt('training_stats.csv',delimiter=',',skiprows=1)
+
+    # Plot the losses and R2s
+    fig, (ax1,ax2) = plt.subplots(nrows=1,ncols=2,figsize=(10,4))
+    ax1.semilogy(data[:,0],label='Training')
+    ax1.semilogy(data[:,1],label='Validation')
+    ax1.set_xlabel('Epochs',fontsize=16)
+    ax1.set_ylabel('Loss',fontsize=16)
+    ax1.legend(fontsize=16)
+    ax1.tick_params(labelsize=12)
+
+    ax2.plot(data[:,2],label='Training')
+    ax2.plot(data[:,3],label='Validation')
+    ax2.set_xlabel('Epochs',fontsize=16)
+    ax2.set_ylabel('Coefficient of determination',fontsize=16)
+    ax2.legend(fontsize=16)
+    ax2.tick_params(labelsize=12)
+    ax2.set_ylim((0.0,1.1))
+    
+    plt.tight_layout()
+    plt.savefig('DNN_Training.pdf')
+    plt.close()
+
+
+def plot_scatter(true,predicted,fname):
+    plt.figure()
+    plt.plot(true,true,label='True')
+    plt.scatter(true,predicted,s=3,label='Predicted',color='red')
+    plt.xlabel('True '+fname,fontsize=16)
+    plt.ylabel('Predicted '+fname,fontsize=16)
+    plt.legend(fontsize=16)
+    plt.tick_params(labelsize=12)
+    plt.savefig(fname+'_scatter.pdf')
+    plt.close()
+
+
 if __name__ == '__main__':
+
     print('Checking plotting of airfoil')
     t_base = np.asarray([0.1268, 0.467, 0.5834, 0.2103, -0.1268, -0.5425, -0.5096, 0.0581])
     t_lower = np.asarray([-0.08876, -0.3269, -0.40838, -0.14721, 0.1648, 0.70525, 0.66248, -0.04067])
     t_upper = np.asarray([0.1648, 0.6071, 0.75842, 0.27339, -0.08876, -0.37975, -0.35672, 0.07553])
     t_truth = np.asarray([0.1009, 0.3306, 0.6281, 0.1494, -0.1627, -0.6344, -0.5927, 0.0421])
-    # t_dnn = np.asarray([0.08876,  0.33483461, 0.57226083, 0.16212897, -0.1648, -0.61612045, -0.53867441,  0.04067]) # 200 training data points
-    t_dnn = np.asarray([0.08876, 0.33675902, 0.5756922, 0.15189275, -0.1648, -0.61284773, -0.53884293, 0.04067]) # 100 training data points
-    # t_dnn = np.asarray([0.09424003, 0.33285433, 0.42598738, 0.19814173, -0.16330369, -0.69081884, -0.37384828,  0.0428294]) # with tol off and ftol on
-    t_rl = np.asarray([0.0887,  0.3269,  0.40838, 0.14721, -0.1648, -0.70525, -0.66248,  0.04067])
+    t_dnn = np.asarray([0.08876, 0.34271419, 0.43059023, 0.14721, -0.1648, -0.62865158, -0.55052489, 0.04578268]) # 30-30 
+    # t_dnn = np.asarray([0.08876, 0.3496284, 0.40838,  0.14721, -0.1648, -0.68837999, -0.57474939,  0.04067]) # 15-15
+    t_bo = np.asarray([0.1016,  0.3280,  0.4131,  0.1701, -0.1602, -0.6980, -0.4652,  0.0468])
+
+    # DNN + BO
+    t_dnn_bo = np.asarray([ 0.0957,  0.3493,  0.6934,  0.1560, -0.1389, -0.5759, -0.6443,  0.0411])
+
+    # DNN lift constrained (lift of 0.9)
+    # t_dnn_lift = np.asarray([0.08876, 0.34957082, 0.52368963, 0.14721, -0.1646667, -0.55007067, -0.50458895, 0.07454936])
+
+    # DNN lift constrained (lift of 1.0)
+    # t_dnn_lift = np.asarray([0.08876, 0.3269, 0.75842, 0.20406465, -0.1648, -0.64436213, -0.56695343, 0.05696154]) # 30-30
+    t_dnn_lift = np.asarray([0.08876, 0.3269, 0.52007714, 0.27339, -0.1648, -0.70525, -0.61496747,  0.07553]) #15-15
 
     # Lower bound
     pnum = 'lower'
@@ -116,10 +172,18 @@ if __name__ == '__main__':
     shape_return(t_upper,pnum)
 
     # Comparison
-    pnum= 'Comparison_DNN'
+    pnum= 'Comparison: DNN'
     shape_return_comparison(t_truth,t_dnn,pnum)
 
     # Comparison
-    pnum= 'Comparison_RL'
-    shape_return_comparison(t_truth,t_rl,pnum)
+    pnum= 'Comparison: BO'
+    shape_return_comparison(t_truth,t_bo,pnum)
+
+    # Comparison
+    pnum= 'Comparison: DNN + BO'
+    shape_return_comparison(t_truth,t_bo,pnum)
+
+    # Comparison
+    pnum= 'Comparison: DNN Lift constrained'
+    shape_return_comparison(t_truth,t_dnn_lift,pnum)
 