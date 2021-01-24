34c5fe018ffb349481d07dae29f17b12276d4d73
Romit Maulik <rmaulik@anl.gov>
2021-01-24 16:32:33 -0600

    Added NN reconstruction from Kernel space

--- Kernel_POD/data_splitter.py
+++ Kernel_POD/data_splitter.py
@@ -1,12 +1,13 @@
 import numpy as np
 np.random.seed(10)
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 Rnum = 2000.0
 x = np.linspace(0.0,1.0,num=256)
 dx = 1.0/np.shape(x)[0]
 
-tsteps = np.linspace(0.0,2.0,num=400)
+tsteps = np.linspace(0.0,2.0,num=800)
 dt = 2.0/np.shape(tsteps)[0]
 
 #-------------------------------------------------------------------------------------------------
@@ -20,16 +21,16 @@ def exact_solution(t):
     return (x/(t+1))/(1.0+np.sqrt((t+1)/t0)*np.exp(Rnum*(x*x)/(4.0*t+4)))
 
 def collect_snapshots():
-    snapshot_matrix = np.zeros(shape=(np.shape(x)[0],np.shape(tsteps)[0]))
+    snapshot_matrix_total = np.zeros(shape=(np.shape(x)[0],np.shape(tsteps)[0]))
 
     trange = np.arange(np.shape(tsteps)[0])
     for t in trange:
-        snapshot_matrix[:,t] = exact_solution(tsteps[t])[:]
+        snapshot_matrix_total[:,t] = exact_solution(tsteps[t])[:]
 
-    snapshot_matrix_mean = np.mean(snapshot_matrix,axis=1)
-    snapshot_matrix = (snapshot_matrix.transpose()-snapshot_matrix_mean).transpose()
+    snapshot_matrix_mean = np.mean(snapshot_matrix_total,axis=1)
+    snapshot_matrix = (snapshot_matrix_total.transpose()-snapshot_matrix_mean).transpose()
 
-    return snapshot_matrix, snapshot_matrix_mean
+    return snapshot_matrix, snapshot_matrix_mean, snapshot_matrix_total
 
 # Method of snapshots to accelerate
 def method_of_snapshots(Y): #Mean removed
@@ -49,18 +50,24 @@ def method_of_snapshots(Y): #Mean removed
 if __name__ == '__main__':
     
     # Collect data
-    total_data, total_data_mean = collect_snapshots()
+    total_data, total_data_mean, _ = collect_snapshots()
     num_snapshots = total_data.shape[1]
     num_dof = total_data.shape[0]
 
+    randomized = np.arange(num_snapshots)
+    np.random.shuffle(randomized)
+
+    train_data = total_data[:,randomized[:600]]
+    test_data = total_data[:,randomized[600:]]
+
     # Generate serial POD with MOS
-    modes = method_of_snapshots(total_data)
+    modes = method_of_snapshots(train_data)
     # Check that this guy is close to one
     print('Inner product of similar modes summed:',np.sum(np.matmul(modes[:,1:2].T,modes[:,1:2])))
     # Check that this guy is close to zero
     print('Inner product of dissimilar modes summed:',np.sum(np.matmul(modes[:,1:2].T,modes[:,2:3])))
 
-    # Find coefficient evolution
+    # Find coefficient evolution of all data
     num_components = 4
     coeff_evolution = np.matmul(total_data.T,modes[:,:num_components])
 
@@ -71,4 +78,33 @@ if __name__ == '__main__':
     plt.plot(coeff_evolution[:,3],label='Dimension 4')
     plt.legend()
     plt.title('Regular POD coefficient evolutions')
+    plt.show()
+
+    # Learning nonlinear function approximator from POD to reconstruction
+    train_coeffs = np.matmul(train_data.T,modes[:,:num_components])
+    test_coeffs = np.matmul(test_data.T,modes[:,:num_components])
+
+    # Define NN model
+    pod_inputs = tf.keras.Input(shape=(train_coeffs.shape[-1],))
+    x = tf.keras.layers.Dense(30, activation="tanh")(pod_inputs)
+    x = tf.keras.layers.Dense(30, activation="tanh")(x)
+    outputs = tf.keras.layers.Dense(train_data.T.shape[-1])(x)
+
+    model = tf.keras.Model(inputs=pod_inputs, outputs=outputs, name="inverse_image_model")
+
+    model.compile(
+        loss=tf.keras.losses.MeanSquaredError(),
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.0005),
+        )   
+
+    history = model.fit(train_coeffs, train_data.T, batch_size=128, epochs=10000, validation_split=0.1)
+    
+    # Try testing
+    test_reconstruction = model.predict(test_coeffs)
+
+    # Plot the reconstruction
+    plt.figure()
+    plt.plot(test_reconstruction[10,:],label="Predicted")
+    plt.plot(test_data.T[10,:],label="True")
+    plt.legend()
     plt.show()
--- Kernel_POD/kernel_pod.py
+++ Kernel_POD/kernel_pod.py
@@ -3,6 +3,7 @@ np.random.seed(10)
 from data_splitter import collect_snapshots
 from sklearn.metrics.pairwise import rbf_kernel
 import matplotlib.pyplot as plt
+import tensorflow as tf
 
 num_components = 4
 
@@ -36,7 +37,7 @@ def get_components(test_data,train_data,evecs):
     return kernelVals, BetaK
 
 if __name__ == '__main__':
-    total_data, total_data_mean = collect_snapshots()
+    _, total_data_mean, total_data = collect_snapshots()
     total_data = total_data.T # Need to transpose for rbf kernel
 
     num_snapshots = np.shape(total_data)[0]
@@ -45,8 +46,8 @@ if __name__ == '__main__':
     randomized = np.arange(num_snapshots)
     np.random.shuffle(randomized)
 
-    train_data = total_data[randomized[:300]]
-    test_data = total_data[randomized[300:]]
+    train_data = total_data[randomized[:600]]
+    test_data = total_data[randomized[600:]]
 
     K = centerK(rbf_kernel(train_data,gamma=0.1))
 
@@ -79,30 +80,60 @@ if __name__ == '__main__':
     plt.title('K-PCA evolution over time')
     plt.show()
 
-    # Learning pre image
-    # Objective function would be || K x - A BetaK.T ||_2 with A as decision variable (for simplest approach)
+    # Learning nonlinear function approximator from BetaK (train) to reconstruction
+    _, BetaK_train = get_components(train_data,train_data,evecs)
+    # Define NN model
+    kpod_inputs = tf.keras.Input(shape=(BetaK_train.shape[-1],))
+    x = tf.keras.layers.Dense(30, activation="tanh")(kpod_inputs)
+    x = tf.keras.layers.Dense(30, activation="tanh")(x)
+    outputs = tf.keras.layers.Dense(total_data.shape[-1])(x)
 
-    # Learning pre-images from training data alone
-    kernelVals, BetaK_train = get_components(train_data,train_data,evecs)
-    Kx = np.matmul(kernelVals,train_data)
+    model = tf.keras.Model(inputs=kpod_inputs, outputs=outputs, name="inverse_image_model")
 
-    # Optimizing for A in num_components x num_dof
-    def residual(A):
-        A = A.reshape(num_components,num_dof)
-        return np.sum((Kx - np.matmul(BetaK_train,A))**2)
+    model.compile(
+        loss=tf.keras.losses.MeanSquaredError(),
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.0005),
+        )   
 
-    callback_array = np.zeros(shape=(1,num_components*num_dof))
-    def callbackF(Xi):
-        global callback_array
-        sol_array = np.copy(Xi)
-        callback_array = np.concatenate((callback_array,sol_array.reshape(1,-1)),axis=0)
+    history = model.fit(BetaK_train, train_data, batch_size=128, epochs=10000, validation_split=0.1)
+    
+    # Try testing
+    _, BetaK_test = get_components(test_data,train_data,evecs)
 
-    from scipy.optimize import minimize
-    solution = minimize(residual,np.zeros(shape=(num_components*num_dof)),method='L-BFGS-B',
-                            tol=1e-8,options={'disp': True, 'maxfun':10000000, 'eps': 1.4901161193847656e-8}, 
-                            callback=callbackF)
+    test_reconstruction = model.predict(BetaK_test)
 
-    Aopt = solution.x
-    print(Aopt.reshape(num_components,num_dof))
+    # Plot the reconstruction
+    plt.figure()
+    plt.plot(test_reconstruction[10,:],label="Predicted")
+    plt.plot(test_data[10,:],label="True")
+    plt.legend()
+    plt.show()
 
-    np.save('Optimized_Preimage.npy',Aopt.reshape(num_components,num_dof))
+
+    # # Learning linear pre image
+    # # Objective function would be || K x - A BetaK.T ||_2 with A as decision variable (for simplest approach)
+
+    # # Learning pre-images from training data alone
+    # kernelVals, BetaK_train = get_components(train_data,train_data,evecs)
+    # Kx = np.matmul(kernelVals,train_data)
+
+    # # Optimizing for A in num_components x num_dof
+    # def residual(A):
+    #     A = A.reshape(num_components,num_dof)
+    #     return np.sum((Kx - np.matmul(BetaK_train,A))**2)
+
+    # callback_array = np.zeros(shape=(1,num_components*num_dof))
+    # def callbackF(Xi):
+    #     global callback_array
+    #     sol_array = np.copy(Xi)
+    #     callback_array = np.concatenate((callback_array,sol_array.reshape(1,-1)),axis=0)
+
+    # from scipy.optimize import minimize
+    # solution = minimize(residual,np.zeros(shape=(num_components*num_dof)),method='L-BFGS-B',
+    #                         tol=1e-8,options={'disp': True, 'maxfun':10000000, 'eps': 1.4901161193847656e-8}, 
+    #                         callback=callbackF)
+
+    # Aopt = solution.x
+    # print(Aopt.reshape(num_components,num_dof))
+
+    # np.save('Optimized_Preimage.npy',Aopt.reshape(num_components,num_dof))
