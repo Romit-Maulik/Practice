67e87261254707b7c4cc950fb29fb0c6c80aa5d9
Romit-Maulik <rmaulik@anl.gov>
2021-07-12 12:23:21 -0500

    Added PDB MWE

---
+++ Other_Python/pdb_tutorial/example.py
@@ -0,0 +1,25 @@
+import numpy as np
+import tensorflow as tf
+import pdb
+
+  
+pdb.set_trace()
+x = np.random.uniform()
+y = np.random.uniform()
+pdb.set_trace()
+
+sumval = x+y
+
+print(sumval)
+
+
+'''
+Run using python example.py
+The pdb.set_trace() line is where a breakpoint is added.
+Commands:
+1. `next` - move to next line
+2. `p something` - check the value of the variable something.
+3. `whatis something` - check the type of variable something.
+4. `c` - continue to next breakpoint
+5. `ctrl-d` - exit pdb.
+'''
